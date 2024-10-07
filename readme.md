36a25a1f2591c05ccc84ed924d2493de523c7f48
SkyBlyvion <richtofen60@gmail.com>
2024-10-07 11:20:42 +0200

    fin du cours et debut exercice

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Commandes
+
+
+
+Compiler = docker exec -it javaone javac Main.java
+
+Lancer = docker exec -it javaone java Main Nico ( Nico est l'argument )
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,35 @@ public class Main {
     // Methode principale et on ajoute un argument
     public static void main(String[] args) {
 
-        // Declaration d'une variable
+        // Declaration de variable int et affecter une valeur
         int age;
-        age = 18;
-        Long caEntreprise = 25600000L;
+        age = 24;
 
+        // long represente un entier
+        long caEntreprise = 25600000L;
+
+        // true ou false
+        boolean isExist = true;
+
+        // Declaration de variables char
+        char leS = 'S';
+        char initial = '\uf224';
+
+        // Classes Integer, String et Boolean encapsulent les primitives
+        Integer codePorte = 4586;
+        String uneChaine = "Le soleil brille";
+        Boolean unTest = false;
+        Integer moiDansLAvenir = age + 15;
 
         // Affichage d'un message dans la console + concat avec l'argument
         System.out.println("New Vegas " + args[0] + "Rocks !");
+        System.out.println("Vous avez " + moiDansLAvenir + " ans");
+        System.out.println("La ca de l'entreprise est de " + caEntreprise);
+        System.out.println("Le char est " + leS);
+        System.out.println("Le char est " + initial);
+        System.out.println("Le code porte est " + codePorte.equals(4586));
+        System.out.println(uneChaine.toUpperCase());
+        System.out.println("Le test est " + unTest);
+
     }
 }
