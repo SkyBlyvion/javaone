273cb54cbf1439f860f35f55d8b661e357c5787d
SkyBlyvion <richtofen60@gmail.com>
2024-10-07 12:10:32 +0200

    exercice collection

--- src/Collection.java
+++ src/Collection.java
@@ -1,3 +1,8 @@
+// on importe java.util.List pour notre test et java.util.ArrayList
+// ou sinon import java.util.*
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collection {
     public static void main(String[] args) {
 
@@ -5,8 +10,16 @@ public class Collection {
         int[] ages = { 5, 15, 18, 22, 55 };
 
         // collections
+        List<String> eleves = new ArrayList<String>();
+        eleves.add("Nicolas");
+        eleves.add("Alexandre");
+        eleves.add("Bob Lennon");
 
+        // print simple
+        System.out.println("Age à l'index 2 : " + ages[2]);
 
-        // age[2].assertEquals(18);
+        // get permet de récupérer une valeur
+        System.out.println("Eleve à l'index 3 : " + eleves.get(2));
+
     }
 }
