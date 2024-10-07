57c751a9213d29e55947bc9062a0e95895809b7f
SkyBlyvion <richtofen60@gmail.com>
2024-10-07 10:57:18 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 public class Main {
     // Methode principale et on ajoute un argument
     public static void main(String[] args) {
+
+        // Declaration d'une variable
+        int age;
+        age = 18;
+        Long caEntreprise = 25600000L;
+
+
         // Affichage d'un message dans la console + concat avec l'argument
         System.out.println("New Vegas " + args[0] + "Rocks !");
     }
